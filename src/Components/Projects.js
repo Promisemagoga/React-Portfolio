d74528bcca8f38cea0f2badeaf7b60f6434f05db
promisemagoga <tshegofatsopromise362@gmail.com>
2023-08-18 07:08:01 +0200

    18/08/2023

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -9,24 +9,24 @@ function Projects() {
         <div>
             <NavBar />
             <div className='projectMain'>
-            <div className='projects project_cards'>
-                <h1>Projects</h1>
-                {projectsData.map((data) => (
-                        <div class="card project-card" style={{width: "18rem;"}}>
-                        <img class="card-img-top" src={data.img} alt="Card image cap" />
-                        <div class="card-body">
-                            <h5 class="card-title">{data.name}</h5>
-                            <p class="card-text">{data.description}</p>
-                            <a href="#" class="btn btn-primary"><Link to={data.buttonLink}>View Project</Link></a>
+                <div className='projects project_cards'>
+                    <h1>Projects</h1>
+                    {projectsData.map((data) => (
+                        <div class=" project-card" >
+                            <img class="card-img-top" src={data.img} alt="Card image cap" />
+                            <div class="card-body">
+                                <h5 class="card-title">{data.name}</h5>
+                                <p class="card-text">{data.description}</p>
+                                <Link class="btn" to={data.buttonLink}>View Project</Link>
+                            </div>
                         </div>
-                    </div>
-                ))}
-            </div>
-            <div className='aboveFooterInfo'>
-                <h3>Let's build something together</h3>
-                <button className='contactMe'>Contact Me</button>
-            </div>
-            <Footer />
+                    ))}
+                </div>
+                <div className='aboveFooterInfo'>
+                    <h3>Let's build something together</h3>
+                    <button className='contactMe'>Contact Me</button>
+                </div>
+                <Footer />
             </div>
         </div>
     )
