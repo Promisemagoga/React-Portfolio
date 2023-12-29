cc2834f2b9ecd0dadbc9f0283521cb0ae64c039c
Promise <tshegofatsopromise362@gmail.com>
2023-12-29 15:23:51 +0200

    29/12/2023

--- src/projectsData.js
+++ src/projectsData.js
@@ -20,33 +20,48 @@ export default [
         img: "limitless.png",
         buttonLink: "https://promisemagoga.github.io/Limitless-Horison/"
     },
+
     {
-        name: "Air-bnb",
-        description: " I cloned airbnb to put my reactjs skills in use.",
-        img: "airbnb-clone.png",
-        buttonLink: "https://air-bnb-clone-vert.vercel.app/"
+        name: "TasteTrails",
+        description: " A resturant app made with react native. To try it out, download expo application on your phone and insert this link: Android:'exp://u.expo.dev/update/5eebc198-996e-49de-912c-c389393c2ed9'  ||  IOS: 'exp://u.expo.dev/update/f8b7e2bb-c5bf-476a-98bc-2150f19a94d1' ",
+        img: "foodApp.png",
+        buttonLink: "#"
 
     },
+    // {
+    //     name: "Air-bnb",
+    //     description: " I cloned airbnb to put my reactjs skills in use.",
+    //     img: "airbnb-clone.png",
+    //     buttonLink: "https://air-bnb-clone-vert.vercel.app/"
+
+    // },
+    // {
+    //     name: "Untrained Chefs",
+    //     description: "are you around jukulyn and feeling like feeding your cravings?Visit untained chefs.Untrained chefs is a local resturant located at soshanguve block K",
+    //     img: "untrainedChefs.jpeg",
+    //     buttonLink: "https://untrained-chefs.webflow.io/"
+
+    // },
     {
-        name: "Untrained Chefs",
-        description: "are you around jukulyn and feeling like feeding your cravings?Visit untained chefs.Untrained chefs is a local resturant located at soshanguve block K",
-        img: "untrainedChefs.jpeg",
+        name: "Shoping-List",
+        description: "Making your shopping easy with Shoping-List Application.To try it out, download expo application on your phone and insert this link: Android:'exp://u.expo.dev/update/0f6d82f0-ac8d-409f-a3d3-34c33ed0bd50'  ||  IOS: 'exp://u.expo.dev/update/803745c4-0a71-4b9f-929e-8b20d5a29c2f' ",
+        img: "shopping.png",
         buttonLink: "https://untrained-chefs.webflow.io/"
 
     },
   
-    {
-        name: "Todo-List software",
-        description: "Manage your job and study with easy. These todo-list allows you to manage your task according to it's priority.",
-        img: "todolist.png",
-        buttonLink: "https://todo-list-app-beta-dun.vercel.app/"
+    // {
+    //     name: "Todo-List software",
+    //     description: "Manage your job and study with easy. These todo-list allows you to manage your task according to it's priority.",
+    //     img: "todolist.png",
+    //     buttonLink: "https://todo-list-app-beta-dun.vercel.app/"
 
-    },
-    {
-        name: "Employee-Tracker Software",
-        description: "Track all the employees in the building",
-        img: "employee.png",
-        buttonLink: "https://employee-tracker-app-lyart.vercel.app/"
+    // },
+    // {
+    //     name: "Employee-Tracker Software",
+    //     description: "Track all the employees in the building",
+    //     img: "employee.png",
+    //     buttonLink: "https://employee-tracker-app-lyart.vercel.app/"
 
-    },
+    // },
 ]
