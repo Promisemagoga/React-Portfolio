5b8048fee1b5dd15f5a0fdd2eff4502613a89d9c
Promise <tshegofatsopromise362@gmail.com>
2023-12-30 11:54:53 +0200

    30/12/2023

--- src/Components/Direction2.js
+++ src/Components/Direction2.js
@@ -4,7 +4,7 @@ function Direction2() {
   return (
 <div className='direction'>
             <h1>Directions!!!</h1>
-            <h4>To test it out, download the expo application app and enter this link:</h4>
+            <h4>To test it out, download the expo application and enter this link:</h4>
             <ul>
                 <li>Android: 'exp://u.expo.dev/update/0f6d82f0-ac8d-409f-a3d3-34c33ed0bd50'  </li>
                 <li> IOS: 'exp://u.expo.dev/update/803745c4-0a71-4b9f-929e-8b20d5a29c2f'</li>
